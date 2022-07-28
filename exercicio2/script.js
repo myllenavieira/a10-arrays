42e2ecd798ec932cad84652def066e1d89e2c756
myllenavieira <vieirasilvamyllena@gmail.com>
2022-07-27 22:49:28 -0300

    Exercícios para praticar A010

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const numerosAleatorios = [34, 87, 139, 7, 234, 12, 0, 76];
+const nomesAleatorios = ["Zan", "Adriana", "Maria", "Victor", "Lucas"];
+const datasAleatorias = ["julho", 1997, false, "maio", "abril", 27, true, 2020];
+console.log(`Quantidade de caracteres do primeiro array: ${numerosAleatorios.length}
+Primeiro item: ${numerosAleatorios[0]}
+O primeiro array tem o número 139? ${numerosAleatorios.includes(139)}
+Quantidade de caracteres do segundo array: ${nomesAleatorios.length}
+Segundo item: ${numerosAleatorios[1]}
+Quantidade de caracteres do terceiro array: ${datasAleatorias.length}
+Terceiro item: ${numerosAleatorios[2]}
+O terceiro array tem o nome Adriana? ${numerosAleatorios.includes("Adriana")}`);
